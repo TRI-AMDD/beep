3006227b1a391ae0dea8f3f6c5f73f982602bb57
Joseph Montoya <joseph.montoya@tri.global>
2020-02-26 13:50:34 -0800

    add TODO for test file dir

--- beep/run_model.py
+++ beep/run_model.py
@@ -600,6 +600,7 @@ def main():
     try:
         args = docopt(__doc__)
         input_json = args['INPUT_JSON']
+        # TODO: resolve test file dir
         if args['--fit']:
             print(process_file_list_from_json(input_json, predict_only=False, model_dir=TEST_FILE_DIR), end="")
         else:
