b709f5f860b2a62988a5b1e76ede730d6158a2b9
Joseph Montoya <joseph.montoya@tri.global>
2020-05-01 10:42:11 -0700

    update and consolidate tests for reorg

--- beep/tests/test_protocol.py
+++ beep/tests/test_protocol.py
@@ -6,9 +6,12 @@ import unittest
 import json
 import boto3
 import datetime
-from beep import PROCEDURE_TEMPLATE_DIR
+from beep.protocol import PROCEDURE_TEMPLATE_DIR, SCHEDULE_TEMPLATE_DIR
 from beep.generate_protocol import Procedure, \
     generate_protocol_files_from_csv
+from beep.protocol.arbin import Schedule
+from beep.protocol.maccor_to_arbin import ProcedureToSchedule
+from beep.tests.test_maccor_to_arbin import TEST_FILE_DIR
 from monty.tempfile import ScratchDir
 from monty.serialization import dumpfn, loadfn
 from monty.os import makedirs_p
@@ -173,3 +176,133 @@ class ProcedureTest(unittest.TestCase):
                  "mode": self.events_mode})
             os.system("generate_protocol {}".format(os_format(json_input)))
             self.assertEqual(len(os.listdir(procedures_path)), 3)
+
+
+class ProcedureToScheduleTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_single_step_conversion(self):
+        procedure = Procedure()
+
+        templates = PROCEDURE_TEMPLATE_DIR
+
+        test_file = 'diagnosticV3.000'
+        json_file = 'test.json'
+
+        proc_dict, sp = procedure.to_dict(os.path.join(templates, test_file),
+                                          os.path.join(templates, json_file)
+                                          )
+        proc_dict = procedure.maccor_format_dict(proc_dict)
+        test_step_dict = proc_dict['MaccorTestProcedure']['ProcSteps']['TestStep']
+
+        converter = ProcedureToSchedule(test_step_dict)
+        step_index = 5
+        step_name_list, step_flow_ctrl = converter.create_metadata()
+
+        self.assertEqual(step_flow_ctrl[7], '5-reset cycle C/20')
+        self.assertEqual(step_flow_ctrl[68], '38-reset cycle')
+
+        step_arbin = converter.compile_to_arbin(test_step_dict[step_index], step_index, step_name_list, step_flow_ctrl)
+        self.assertEqual(step_arbin['m_szLabel'], '6-None')
+        self.assertEqual(step_arbin['[Schedule_Step5_Limit0]']['m_szGotoStep'], 'Next Step')
+        self.assertEqual(step_arbin['[Schedule_Step5_Limit0]']['Equation0_szLeft'], 'PV_CHAN_Voltage')
+        self.assertEqual(step_arbin['[Schedule_Step5_Limit2]']['m_szGotoStep'], '70-These are the 2 reset cycles')
+
+        step_index = 8
+        step_arbin = converter.compile_to_arbin(test_step_dict[step_index], step_index, step_name_list, step_flow_ctrl)
+        print(step_index, test_step_dict[step_index])
+        print(step_arbin)
+        self.assertEqual(step_arbin['[Schedule_Step8_Limit0]']['Equation0_szLeft'], 'PV_CHAN_CV_Stage_Current')
+        self.assertEqual(step_arbin['[Schedule_Step8_Limit0]']['Equation0_szRight'],
+                         test_step_dict[step_index]['Ends']['EndEntry'][0]['Value'])
+        os.remove(os.path.join(templates, json_file))
+
+    def test_serial_conversion(self):
+        procedure = Procedure()
+
+        templates = PROCEDURE_TEMPLATE_DIR
+
+        test_file = 'diagnosticV3.000'
+        json_file = 'test.json'
+
+        proc_dict, sp = procedure.to_dict(os.path.join(templates, test_file),
+                                          os.path.join(templates, json_file)
+                                          )
+        proc_dict = procedure.maccor_format_dict(proc_dict)
+        test_step_dict = proc_dict['MaccorTestProcedure']['ProcSteps']['TestStep']
+
+        converter = ProcedureToSchedule(test_step_dict)
+        step_name_list, step_flow_ctrl = converter.create_metadata()
+
+        for step_index, step in enumerate(test_step_dict):
+            if 'Loop' in step['StepType']:
+                print(step_index, step)
+            step_arbin = converter.compile_to_arbin(test_step_dict[step_index], step_index,
+                                                    step_name_list, step_flow_ctrl)
+            if 'Loop' in step['StepType']:
+                self.assertEqual(step_arbin['m_szStepCtrlType'], 'Set Variable(s)')
+                self.assertEqual(step_arbin['m_uLimitNum'], '2')
+            if step_index == 15:
+                self.assertEqual(step_arbin['[Schedule_Step15_Limit0]']['m_szGotoStep'], '11-None')
+                self.assertEqual(step_arbin['[Schedule_Step15_Limit1]']['m_szGotoStep'], 'Next Step')
+        os.remove(os.path.join(templates, json_file))
+
+    def test_schedule_creation(self):
+        procedure = Procedure()
+
+        templates = PROCEDURE_TEMPLATE_DIR
+
+        test_file = 'diagnosticV3.000'
+        json_file = 'test.json'
+        sdu_test_input = os.path.join(SCHEDULE_TEMPLATE_DIR, '20170630-3_6C_9per_5C.sdu')
+        sdu_test_output = os.path.join(TEST_FILE_DIR, 'schedule_test_output.sdu')
+
+        proc_dict, sp = procedure.to_dict(os.path.join(templates, test_file),
+                                          os.path.join(templates, json_file)
+                                          )
+        proc_dict = procedure.maccor_format_dict(proc_dict)
+        test_step_dict = proc_dict['MaccorTestProcedure']['ProcSteps']['TestStep']
+
+        converter = ProcedureToSchedule(test_step_dict)
+        converter.create_sdu(sdu_test_input, sdu_test_output)
+        os.remove(os.path.join(templates, json_file))
+        os.remove(sdu_test_output)
+
+
+class ArbinScheduleTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_dict_to_file(self):
+        filename = '20170630-3_6C_9per_5C.sdu'
+        schedule = Schedule.from_file(os.path.join(SCHEDULE_TEMPLATE_DIR, filename))
+        testname = 'test1.sdu'
+        with ScratchDir('.'):
+            dumpfn(schedule, "schedule_test.json")
+            schedule.to_file(testname)
+            hash1 = hash_file(os.path.join(SCHEDULE_TEMPLATE_DIR, filename))
+            hash2 = hash_file(testname)
+            if hash1 != hash2:
+                original = open(os.path.join(SCHEDULE_TEMPLATE_DIR, filename), encoding='latin-1').read()
+                parsed = open(testname, encoding='latin-1').read()
+                self.assertFalse(list(difflib.unified_diff(original, parsed)))
+                for line in difflib.unified_diff(original, parsed):
+                    print(line)
+
+    def test_fastcharge(self):
+        filename = '20170630-3_6C_9per_5C.sdu'
+        test_file = 'test.sdu'
+        sdu = Schedule.from_fast_charge(1.1 * 3.6, 0.086, 1.1 * 5, os.path.join(SCHEDULE_TEMPLATE_DIR, filename))
+        with ScratchDir('.'):
+            sdu.to_file(test_file)
+            hash1 = hash_file(os.path.join(SCHEDULE_TEMPLATE_DIR, filename))
+            hash2 = hash_file(test_file)
+            if hash1 != hash2:
+                original = open(os.path.join(SCHEDULE_TEMPLATE_DIR, filename), encoding='latin-1').readlines()
+                parsed = open(test_file, encoding='latin-1').readlines()
+                udiff = list(difflib.unified_diff(original, parsed))
+                for line in udiff:
+                    print(line)
+                self.assertFalse(udiff)
