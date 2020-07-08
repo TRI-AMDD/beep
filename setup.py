354d1bdde0a0acc484b5004a9bac337ad1f26a32
Patrick Herring <patrick.herring@tri.global>
2020-07-08 11:51:17 -0400

    v2020.7.8 release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ arbin and maccor cyclers.
 
 setup(name="beep",
       url="https://github.com/TRI-AMDD/beep",
-      version="2020.6.2",
+      version="2020.7.8",
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       long_description_content_type='text/markdown',
