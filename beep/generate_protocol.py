1c5d52a3550550e7b3ac8f4bab493dc1aa0c1cff
Patrick Herring <patrick.herring@tri.global>
2020-09-22 22:25:00 -0400

    Fix template name passing

--- beep/generate_protocol.py
+++ beep/generate_protocol.py
@@ -118,8 +118,10 @@ def generate_protocol_files_from_csv(csv_filename, output_directory=None):
                 diag_params_df["diagnostic_parameter_set"]
                 == protocol_params["diagnostic_parameter_set"]
             ].squeeze()
-
-            protocol = Procedure.generate_procedure_regcyclev3(index, protocol_params)
+            template_fullpath = os.path.join(PROCEDURE_TEMPLATE_DIR, template)
+            protocol = Procedure.generate_procedure_regcyclev3(index,
+                                                               protocol_params,
+                                                               template=template_fullpath)
             protocol.generate_procedure_diagcyclev3(
                 protocol_params["capacity_nominal"], diagnostic_params
             )
