d0796f313332956c0d303af5906b48d0ebca0f0d
ardunn <ardunn@lbl.gov>
2021-03-22 15:52:48 -0700

    scaffolding for new structuring subpackage

---
+++ beep/structure/arbin.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+import pytz
+
+from beep.conversion_schemas import ARBIN_CONFIG
+from beep import logger
+from beep.structure.base import BEEPDatapath
+
+
+class ArbinDatapath(BEEPDatapath):
+
+    @classmethod
+    def from_file(cls, path, metadata_path=None):
+        """
+        """
+        data = pd.read_csv(path)
+        data.rename(str.lower, axis="columns", inplace=True)
+
+        for column, dtype in ARBIN_CONFIG["data_types"].items():
+            if column in data:
+                if not data[column].isnull().values.any():
+                    data[column] = data[column].astype(dtype)
+
+        data.rename(ARBIN_CONFIG["data_columns"], axis="columns", inplace=True)
+
+        metadata_path = metadata_path if metadata_path else path.replace(".csv",
+                                                                         "_Metadata.csv")
+
+        if os.path.exists(metadata_path):
+            metadata = pd.read_csv(metadata_path)
+            metadata.rename(str.lower, axis="columns", inplace=True)
+            metadata.rename(ARBIN_CONFIG["metadata_fields"], axis="columns",
+                            inplace=True)
+            # Note the to_dict, which scrubs numpy typing
+            metadata = {col: item[0] for col, item in
+                        metadata.to_dict("list").items()}
+        else:
+            logger.warning(f"No associated metadata file for Arbin: "
+                           f"'{metadata_path}'. No metadata loaded.")
+            metadata = {}
+
+        # standardizing time format
+        data["date_time_iso"] = data["date_time"].apply(
+            lambda x: datetime.utcfromtimestamp(x).replace(
+                tzinfo=pytz.UTC).isoformat()
+        )
+
+        paths = {
+            "raw": path,
+            "metadata": metadata_path if metadata else None
+        }
+
+        return cls(data, metadata, paths)
---
+++ beep/structure/base_eis.py
@@ -0,0 +1,72 @@
+"""
+Base classes for EIS.
+"""
+
+import pandas as pd
+from monty.json import MSONable
+
+from beep.structure.base import BEEPDatapath
+
+
+class EISpectrum(MSONable):
+    """
+    Class describing an Electrochemical Impedance Spectrum
+    """
+
+    def __init__(self, data, metadata):
+        """
+
+        Args:
+            data:
+            metadata:
+        """
+        self.data = data
+        self.metadata = metadata
+
+
+    @classmethod
+    def from_file(cls, filename):
+        raise NotImplementedError
+
+    def as_dict(self):
+        """
+        Method for serialization as dictionary
+
+        Returns:
+            ({}): dictionary representation of this object
+
+        """
+        return {
+            "@module": self.__class__.__module__,
+            "@class": self.__class__.__name__,
+            "data": self.data.to_dict("list"),
+            "metadata": self.metadata.to_dict(),
+        }
+
+    @classmethod
+    def from_dict(cls, d):
+        """
+        Method of invocation from dictionary
+
+        Args:
+            d ({}): dictionary from which to invoke
+
+        Returns:
+            (EISpectrum): object invoked from dictionary
+
+        """
+        data = pd.DataFrame(d["data"])
+        data = data.sort_index()
+        metadata = pd.DataFrame(d["metadata"])
+        return cls(data, metadata)
+
+
+
+class BEEPDatapathWithEIS(BEEPDatapath):
+
+    def __init__(self, *args, **kwargs):
+        self.eis = None
+        super(BEEPDatapathWithEIS, self).__init__(*args, **kwargs)
+
+    def load_eis(self, *args, **kwargs):
+        raise NotImplementedError("EIS containing datapath must implement 'load_eis' method.")
