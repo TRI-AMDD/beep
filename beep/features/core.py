f2c054f9e78aec8e00700a7b60b2bf54a1f381c8
ardunn <ardunn@lbl.gov>
2021-09-02 16:36:49 -0700

    add more docs to diagnosticproperties

--- beep/features/core.py
+++ beep/features/core.py
@@ -720,6 +720,26 @@ class DiagnosticProperties(BEEPFeaturizer):
         X (pandas.DataFrame): features in DataFrame format.
         metadata (dict): information about the conditions, data
             and code used to produce features
+
+    Hyperparameters:
+        parameters_dir (str): Full path to directory of parameters to analyse the
+            diagnostic cycles
+        quantities ([str]): Quantities to extract/get fractional metrics for
+            diagnostic cycles
+        cycle_type (str): Type of diagnostic cycle being used to measure the
+            fractional metric
+        metric (str): The metric being used for fractional capacity
+        interpolation_axes (list): List of column names to use for
+            x_axis interpolation (distance to threshold)
+        threshold (float): Value for the fractional metric to be considered above
+            or below threshold
+        filter_kinks (float): If set, cutoff value for the second derivative of
+            the fractional metric (cells with an abrupt change in degradation
+            rate might have something else going on). Typical value might be 0.04
+        extrapolate_threshold (bool): Should threshold crossing point be
+            extrapolated for cells that have not yet reached the threshold
+            (warning: this uses a linear extrapolation from the last two
+            diagnostic cycles)
     """
     DEFAULT_HYPERPARAMETERS = {
         "parameters_dir": PROTOCOL_PARAMETERS_DIR,
@@ -776,97 +796,111 @@ class DiagnosticProperties(BEEPFeaturizer):
                 summary_diag_cycle_type.loc[:, "metric"] = quantity
                 X = X.append(summary_diag_cycle_type)
 
-        X_condensed = get_threshold_targets(
-            df=X,
-            cycle_type=self.hyperparameters["cycle_type"],
-            metric=self.hyperparameters["metric"],
-            interpolation_axes=self.hyperparameters["interpolation_axes"],
-            threshold=self.hyperparameters["threshold"],
-            filter_kinks=self.hyperparameters["filter_kinks"],
-            extrapolate_threshold=self.hyperparameters["extrapolate_threshold"]
-        )
+        X_condensed = self.get_threshold_targets(X)
         self.features = X_condensed
-        
 
-def get_threshold_targets(df, cycle_type, metric, interpolation_axes, threshold, filter_kinks, extrapolate_threshold):
-    if filter_kinks and np.any(
-            df['fractional_metric'].diff().diff() < filter_kinks):
-        last_good_cycle = df[
-            df['fractional_metric'].diff().diff() < filter_kinks][
-            'cycle_index'].min()
-        df = df[
-            df['cycle_index'] < last_good_cycle]
+    def get_threshold_targets(self, df):
+        """
+        Apply a threshold via interpolation for determining various
+        metrics (e.g., discharge energy) from diagnostic cycles.
 
-    x_axes = []
-    for type in interpolation_axes:
-        x_axes.append(df[type])
-    y_interpolation_axis = df['fractional_metric']
+        Args:
+            df (pd.DataFrame): A dataframe of diagnostic cycle data
+                for a single battery cycler run.
 
-    # Logic around how to deal with cells that have not crossed the threshold
-    if df['fractional_metric'].min() > threshold and not extrapolate_threshold:
-        BEEPFeaturizationError(
-            "DiagnosticProperties data has not crossed threshold and extrapolation inaccurate"
-        )
-    elif df['fractional_metric'].min() > threshold and extrapolate_threshold:
-        fill_value = "extrapolate"
-        bounds_error = False
-        x_linspaces = []
-        for x_axis in x_axes:
-            y1 = y_interpolation_axis.iloc[-2]
-            y2 = y_interpolation_axis.iloc[-1]
-            x1 = x_axis.iloc[-2]
-            x2 = x_axis.iloc[-1]
-            x_thresh_extrap = (threshold - 0.1 - y1) * (x2 - x1) / (
-                        y2 - y1) + x1
-            x_linspaces.append(
-                np.linspace(x_axis.min(), x_thresh_extrap, num=1000)
+        Returns:
+            (pd.DataFrame): Contains a vector for interpolated/intercept
+                data for determining threshold.
+
+        """
+        cycle_type = self.hyperparameters["cycle_type"],
+        metric = self.hyperparameters["metric"],
+        interpolation_axes = self.hyperparameters["interpolation_axes"],
+        threshold = self.hyperparameters["threshold"],
+        filter_kinks = self.hyperparameters["filter_kinks"],
+        extrapolate_threshold = self.hyperparameters["extrapolate_threshold"]
+
+        if filter_kinks and np.any(
+                df['fractional_metric'].diff().diff() < filter_kinks):
+            last_good_cycle = df[
+                df['fractional_metric'].diff().diff() < filter_kinks][
+                'cycle_index'].min()
+            df = df[
+                df['cycle_index'] < last_good_cycle]
+
+        x_axes = []
+        for type in interpolation_axes:
+            x_axes.append(df[type])
+        y_interpolation_axis = df['fractional_metric']
+
+        # Logic around how to deal with cells that have not crossed threshold
+        if df['fractional_metric'].min() > threshold and \
+            not extrapolate_threshold:
+            BEEPFeaturizationError(
+                "DiagnosticProperties data has not crossed threshold "
+                "and extrapolation inaccurate"
             )
-    else:
-        fill_value = np.nan
-        bounds_error = True
-        x_linspaces = []
+        elif df['fractional_metric'].min() > threshold and extrapolate_threshold:
+            fill_value = "extrapolate"
+            bounds_error = False
+            x_linspaces = []
+            for x_axis in x_axes:
+                y1 = y_interpolation_axis.iloc[-2]
+                y2 = y_interpolation_axis.iloc[-1]
+                x1 = x_axis.iloc[-2]
+                x2 = x_axis.iloc[-1]
+                x_thresh_extrap = (threshold - 0.1 - y1) * (x2 - x1) / (
+                            y2 - y1) + x1
+                x_linspaces.append(
+                    np.linspace(x_axis.min(), x_thresh_extrap, num=1000)
+                )
+        else:
+            fill_value = np.nan
+            bounds_error = True
+            x_linspaces = []
+            for x_axis in x_axes:
+                x_linspaces.append(
+                    np.linspace(x_axis.min(), x_axis.max(), num=1000))
+
+        f_axis = []
         for x_axis in x_axes:
-            x_linspaces.append(
-                np.linspace(x_axis.min(), x_axis.max(), num=1000))
-
-    f_axis = []
-    for x_axis in x_axes:
-        f_axis.append(
-            interp1d(
-                x_axis,
-                y_interpolation_axis,
-                kind='linear',
-                bounds_error=bounds_error,
-                fill_value=fill_value
+            f_axis.append(
+                interp1d(
+                    x_axis,
+                    y_interpolation_axis,
+                    kind='linear',
+                    bounds_error=bounds_error,
+                    fill_value=fill_value
+                )
             )
-        )
 
-    x_to_threshold = []
-    for indx, x_linspace in enumerate(x_linspaces):
-        crossing_array = abs(f_axis[indx](x_linspace) - threshold)
-        x_to_threshold.append(x_linspace[np.argmin(crossing_array)])
+        x_to_threshold = []
+        for indx, x_linspace in enumerate(x_linspaces):
+            crossing_array = abs(f_axis[indx](x_linspace) - threshold)
+            x_to_threshold.append(x_linspace[np.argmin(crossing_array)])
 
-    if ~(x_to_threshold[0] > 0) or ~(x_to_threshold[1] > 0):
-        raise BEEPFeaturizationError(
-            "DiagnosticProperties data does not have a positive value to threshold"
-        )
+        if ~(x_to_threshold[0] > 0) or ~(x_to_threshold[1] > 0):
+            raise BEEPFeaturizationError(
+                "DiagnosticProperties data does not have a positive value "
+                "to threshold"
+            )
 
-    if "normalized_regular_throughput" in interpolation_axes:
-        real_throughput_to_threshold = x_to_threshold[interpolation_axes.index(
-            "normalized_regular_throughput")] * \
-                                       df[
-                                           'initial_regular_throughput'].values[
-                                           0]
-        x_to_threshold.append(real_throughput_to_threshold)
-        interpolation_axes.append("real_regular_throughput")
+        if "normalized_regular_throughput" in interpolation_axes:
+            real_throughput_to_threshold = x_to_threshold[interpolation_axes.index(
+                "normalized_regular_throughput")] * \
+                                           df[
+                                               'initial_regular_throughput'].values[
+                                               0]
+            x_to_threshold.append(real_throughput_to_threshold)
+            interpolation_axes.append("real_regular_throughput")
 
-    threshold_dict = {
-        'initial_regular_throughput':
-            df['initial_regular_throughput'].values[0],
-    }
+        threshold_dict = {
+            'initial_regular_throughput':
+                df['initial_regular_throughput'].values[0],
+        }
 
-    for indx, x_axis in enumerate(interpolation_axes):
-        threshold_dict[cycle_type + metric + str(threshold) + '_' + x_axis] = [
-            x_to_threshold[indx]]
+        for indx, x_axis in enumerate(interpolation_axes):
+            threshold_dict[cycle_type + metric + str(threshold) + '_' + x_axis] = [
+                x_to_threshold[indx]]
 
-    return pd.DataFrame(threshold_dict)
+        return pd.DataFrame(threshold_dict)
