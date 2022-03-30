f0778f9b41abfce172507cd66a0da676ac97b912
ardunn <ardunn@lbl.gov>
2022-03-29 19:22:28 -0700

    refactoring featurizers 2

--- beep/features/all_cycles/summary.py
+++ beep/features/all_cycles/summary.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 from scipy.stats import skew, kurtosis
 
+from beep import PROTOCOL_PARAMETERS_DIR
 from beep.features.featurizer import BEEPAllCyclesFeaturizer
-
 
 
 class CycleSummaryStats(BEEPAllCyclesFeaturizer):
