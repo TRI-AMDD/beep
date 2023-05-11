8e785a6342e897c2666b569dddc113b1a06970e1
ardunn <ardunn@lbl.gov>
2023-05-10 22:30:54 -0700

    update Run to include dask bags inside cyclecontainers

--- beep/structure/arbin.py
+++ beep/structure/arbin.py
@@ -8,13 +8,10 @@ import pytz
 from monty.serialization import loadfn
 import pandas as pd
 
-from beep.conversion_schemas import ARBIN_CONFIG
-from beep import logger, VALIDATION_SCHEMA_DIR
 from beep.structure.base import BEEPDatapath
-from beep.structure.validate import PROJECT_SCHEMA
+from beep import logger
 
-
-class ArbinDatapath(BEEPDatapath):
+class ArbinRun(BEEPDatapath):
     """A datapath for Arbin cycler data.
 
     Arbin cycler data contains two files:
@@ -27,17 +24,136 @@ class ArbinDatapath(BEEPDatapath):
     Attributes:
         All from BEEPDatapath
     """
+    CONVERSION_CONFIG = {
+        'file_pattern': ".*CH.*\\.csv",
+        'metadata_fields': {
+            'test_id': 'test_id',
+            'device_id': 'device_id',
+            'iv_ch_id': 'channel_id',
+            'first_start_datetime': 'start_datetime',
+            'schedule_file_name': 'protocol',
+            'item_id': 'barcode',
+            'resumed_times': '_resumed_times',
+            'last_end_datetime': '_last_end_datetime',
+            'databases': '_databases',
+            'grade_id': '_grade_id',
+            'has_aux': '_has_aux',
+            'has_special': '_has_special',
+            'schedule_version': '_schedule_version',
+            'log_aux_data_flag': '_log_aux_data_flag',
+            'log_special_data_flag': '_log_special_data_flag',
+            'rowstate': '_rowstate',
+            'canconfig_filename': '_canconfig_filename',
+            'm_ncanconfigmd5': '_m_ncanconfigmd5',
+            'value': '_value',
+            'value2': '_value2'
+        },
+        'data_columns': {
+            'data_point': 'data_point',
+            'test_time': 'test_time',
+            'datetime': 'date_time',
+            'step_time': 'step_time',
+            'step_index': 'step_index',
+            'cycle_index': 'cycle_index',
+            'current': 'current',
+            'voltage': 'voltage',
+            'charge_capacity': 'charge_capacity',
+            'discharge_capacity': 'discharge_capacity',
+            'charge_energy': 'charge_energy',
+            'discharge_energy': 'discharge_energy',
+            'dv/dt': '_dv/dt',
+            'internal_resistance': 'internal_resistance',
+            'temperature': 'temperature'
+        },
+        'data_types': {
+            'data_point': 'int32',
+            'test_time': 'float64',
+            'datetime': 'float32',
+            'step_time': 'float32',
+            'step_index': 'int16',
+            'cycle_index': 'int32',
+            'current': 'float32',
+            'voltage': 'float32',
+            'charge_capacity': 'float64',
+            'discharge_capacity': 'float64',
+            'charge_energy': 'float64',
+            'discharge_energy': 'float64',
+            'dv/dt': 'float32',
+            'internal_resistance': 'float32',
+            'temperature': 'float32'
+        }
+    }
 
-    conversion_config = ARBIN_CONFIG
+    # Default validation schema is based on operating parameters
+    # of lithium iron phosphate cells
+    VALIDATION_SCHEMA = {
+        'charge_capacity': {
+            'schema': {
+                'max': 2.0,
+                'min': 0.0,
+                'type': 'float'
+            },
+            'type': 'list'
+        },
+        'cycle_index': {
+            'schema': {
+                'min': 0,
+                'max_at_least': 1,
+                'type': 'integer'
+            },
+            'type': 'list'
+        },
+        'discharge_capacity': {
+            'schema': {
+                'max': 2.0,
+                'min': 0.0,
+                'type': 'float'
+            },
+            'type': 'list'
+        },
+        'temperature': {
+            'schema': {
+                'max': 80.0,
+                'min': 20.0,
+                'type': 'float'
+            },
+            'type': 'list'
+        },
+        'test_time': {
+            'schema': {
+                'type': 'float'
+            },
+            'type': 'list'
+        },
+        'voltage': {
+            'schema': {
+                'max': 3.8,
+                'min': 0.0,
+                'type': 'float'
+            },
+            'type': 'list'
+        }
+    }
 
     @classmethod
-    def from_file(cls, path, metadata_path=None):
-        """Load an Arbin file to a datapath.
+    def from_file(
+        cls, 
+        path, 
+        metadata_path=None, 
+        validation_schema=None, 
+        extra_metadata=None
+    ):
+        """Load an Arbin file to a Run object.
 
         Args:
             path (str, Pathlike): Path to the raw data csv.
             metadata_path (str, None): Path to metadata file, if it
                 cannot be inferred from the path of the raw file.
+            validation_schema (dict): Validation schema as a dictionary. If none
+                is passed, the default will be used.
+            extra_metadata (dict): Extra metadata to add to the metadata file.
+                For example, if a custom validation dictionary was passed,
+                you can include the filename is was determined from, etc. 
 
         Returns:
             (ArbinDatapath)
@@ -45,12 +161,12 @@ class ArbinDatapath(BEEPDatapath):
         data = pd.read_csv(path, index_col=0)
         data.rename(str.lower, axis="columns", inplace=True)
 
-        for column, dtype in cls.conversion_config["data_types"].items():
+        for column, dtype in cls.CONVERSION_CONFIG["data_types"].items():
             if column in data:
                 if not data[column].isnull().values.any():
                     data[column] = data[column].astype(dtype)
 
-        data.rename(cls.conversion_config["data_columns"], axis="columns", inplace=True)
+        data.rename(cls.CONVERSION_CONFIG["data_columns"], axis="columns", inplace=True)
 
         metadata_path = metadata_path if metadata_path else path.replace(".csv",
                                                                          "_Metadata.csv")
@@ -58,7 +174,7 @@ class ArbinDatapath(BEEPDatapath):
         if os.path.exists(metadata_path):
             metadata = pd.read_csv(metadata_path)
             metadata.rename(str.lower, axis="columns", inplace=True)
-            metadata.rename(cls.conversion_config["metadata_fields"], axis="columns",
+            metadata.rename(cls.CONVERSION_CONFIG["metadata_fields"], axis="columns",
                             inplace=True)
             # Note the to_dict, which scrubs numpy typing
             metadata = {col: item[0] for col, item in
@@ -79,14 +195,15 @@ class ArbinDatapath(BEEPDatapath):
             "metadata": metadata_path if metadata else None
         }
 
-        # Set schema from filename, if possible; otherwise, use default arbin schema
-        project_schema = loadfn(PROJECT_SCHEMA)
-        name = os.path.basename(path)
-        special_schema_filename = project_schema.get(name.split("_")[0], {}).get("arbin")
+        if not validation_schema:
+            validation_schema = cls.VALIDATION_SCHEMA
 
-        if special_schema_filename:
-            schema = os.path.join(VALIDATION_SCHEMA_DIR, special_schema_filename)
-        else:
-            schema = os.path.join(VALIDATION_SCHEMA_DIR, "schema-arbin-lfp.yaml")
+        if extra_metadata:
+            metadata.update(extra_metadata)
 
-        return cls(data, metadata, paths=paths, schema=schema)
+        return cls(
+            data, 
+            metadata, 
+            paths=paths, 
+            schema=validation_schema
+        )
